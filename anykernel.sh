4e52a4d8bb4fab770f05d31cc24feae3aa358217
baibhab <fakebai34@gmail.com>
2020-02-16 08:56:35 +0000

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string Stormbreaker by baibhab
+kernel.string=Stormbreaker by baibhab
 do.devicecheck=1
 do.modules=0
 do.systemless=1

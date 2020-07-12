64ae985d67a44300b7eac910ad7ec5681b18c849
vijaymalav564 <jaymalav10@gmail.com>
2020-07-12 23:33:45 +0530

    Set up for RMX1801

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=0
+kernel.string=LockDown Kernel by Vijay Malav
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=RMX1801
+device.name2=rmx1801
+device.name3=omni_RMX1801
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=

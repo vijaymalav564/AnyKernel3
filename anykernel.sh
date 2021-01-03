d5a16f5025f9fb300b5bfb2018c139ebce0f8da7
vijaymalav564 <jaymalav10@gmail.com>
2021-01-03 21:41:47 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=LockDown Kernel by VijayMalav
+kernel.string=LockDown Kernel by Vijay Malav
 do.devicecheck=1
 do.modules=0
 do.systemless=1
